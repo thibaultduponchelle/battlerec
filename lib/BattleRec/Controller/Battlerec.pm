e11500089b54ade53bd29f58d84a27af9b1e8d32
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2018-12-21 06:41:08 +0100

    V1

--- lib/BattleRec/Controller/Battlerec.pm
+++ lib/BattleRec/Controller/Battlerec.pm
@@ -11,15 +11,18 @@ sub inverser($) {
   if($res eq "V") { return "D"; } elsif($res eq "D") { return "V"; } else { return $res; }
 }
 
-sub derniers($) {
+sub derniers($$) {
   my $mc = shift;
-  my $docs = mango->db("battlerec")->collection("battles")->find({'$or' => [{mc1 => "$mc"}, {mc2 => "$mc"}]})->sort({ date => -1 })->limit(6);
+  my $date = shift;
+
+  print "Date : $date\n";
+  my $docs = mango->db("battlerec")->collection("battles")->find({'$and' => [{date => {'$lt' => "$date"}} , {'$or' => [{mc1 => "$mc"}, {mc2 => "$mc"}]}]} )->sort({ date => -1 })->limit(5);
   my $lasts = "";
   while (my $d = $docs->next) { 
     if($mc eq $d->{mc1}) {
-      $lasts .= $d->{resultat}; 
+      $lasts = $d->{resultat} . $lasts; 
     } else { 
-      $lasts .= inverser($d->{resultat}); 
+      $lasts = inverser($d->{resultat}) . $lasts; 
     }
   }
     
@@ -28,9 +31,11 @@ sub derniers($) {
   return $lasts;
 }
 
-sub record($) {
+sub record($$) {
   my $mc = shift;
-  my $docs = mango->db("battlerec")->collection("battles")->find({'$or' => [{mc1 => "$mc"}, {mc2 => "$mc"}]})->sort({ date => -1 });
+  my $date = shift;
+
+  my $docs = mango->db("battlerec")->collection("battles")->find({'$and' => [{date => {'$lt' => "$date"}} , {'$or' => [{mc1 => "$mc"}, {mc2 => "$mc"}]}]} )->sort({ date => -1 })->limit(5);
   my $v = 0;
   my $d = 0;
   my $n = 0;
@@ -65,24 +70,24 @@ sub mc {
   my $name = $self->stash("name");
   
   my $docs = mango->db("battlerec")->collection("battles")->find({'$or' => [{mc1 => "$name"}, {mc2 => "$name"}]})->sort({ date => -1 });
-  my %derniers = (); 
-  my %balance = (); 
+  my @battles;
   while (my $d = $docs->next) { 
-    if($d->{mc1} eq $name) {
-      $derniers{$d->{mc2}} = derniers($d->{mc2}); 
-      $balance{$d->{mc2}} = record($d->{mc2}); 
-    } else {
-      $derniers{$d->{mc1}} = derniers($d->{mc1}); 
-      $balance{$d->{mc1}} = record($d->{mc1}); 
+    my %battle = ();
+    foreach my $k (keys $d) {
+      $battle{$k} = $d->{$k};
     }
-
+    if($d->{mc1} eq $name) {
+      $battle{derniers} = derniers($d->{mc2}, $d->{date});
+      $battle{balance} = record($d->{mc2}, $d->{date});
+    } else {
+      $battle{derniers} = derniers($d->{mc1}, $d->{date});
+      $battle{balance} = record($d->{mc1}, $d->{date});
+    }
+    push @battles, \%battle;
   } 
-    
-  $balance{$name} = record($name); 
 
-  $docs->rewind;
-  
-  $self->render(mc => $name, battles => $docs, derniers => \%derniers, balance => \%balance);
+  my $balance = record($name, "3099-12-12"); 
+  $self->render(mc => $name, battles => \@battles, balance => $balance);
 }
 
 # This action will render a template
@@ -90,18 +95,23 @@ sub index {
   my $self = shift;
   
   my $docs = mango->db("battlerec")->collection("battles")->find()->sort({ date => -1 })->limit(1000);
+  
+  my @battles;
 
-  my %derniers = (); 
-  my %balance = (); 
   while (my $d = $docs->next) { 
-    $derniers{$d->{mc2}} = derniers($d->{mc2}); 
-    $balance{$d->{mc2}} = record($d->{mc2}); 
-    $derniers{$d->{mc1}} = derniers($d->{mc1}); 
-    $balance{$d->{mc1}} = record($d->{mc1}); 
+    my %battle = ();
+    foreach my $k (keys $d) {
+      $battle{$k} = $d->{$k};
+    }
+    $battle{derniersmc1} = derniers($d->{mc1}, $d->{date});
+    $battle{balancemc1} = record($d->{mc1}, $d->{date});
+    $battle{derniersmc2} = derniers($d->{mc2}, $d->{date});
+    $battle{balancemc2} = record($d->{mc2}, $d->{date});
+    
+    push @battles, \%battle;
   } 
     
-  $docs->rewind;
-  $self->render(battles => $docs, derniers => \%derniers, balance => \%balance);
+  $self->render(battles => \@battles);
 }
 
 1;
