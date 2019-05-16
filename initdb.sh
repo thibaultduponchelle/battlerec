6d52abd5e2a4e4a2623d7cc9489496bf08894b8a
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2019-05-16 16:36:26 +0200

    Classement par nombre de battle. Debut...

--- initdb.sh
+++ initdb.sh
@@ -1,4 +1,5 @@
 mongo battlerec --eval 'db.battles.remove({})'
 mongo battlerec --eval 'db.pbattles.remove({})'
+mongo battlerec --eval 'db.xp.remove({})'
 
 mongoimport -d battlerec -c battles --type csv --file data.csv --headerline
