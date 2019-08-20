2c0730bc7f9ba211df5107b6ffe9faadb79e6b83
Thibault DUPONCHELLE <thibault.duponchelle@amadeus.com>
2019-08-20 05:35:25 -0400

    Setup...

--- setup.sh
+++ setup.sh
@@ -11,6 +11,11 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DC
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 apt-get update
 apt-get install -y mongodb-org
+# Eventually...
+# apt-get install -y mongodb-org-server libcurl3
+# mkdir -p /data/db
+# chown mongodb:mongodb /data/db
+
 service mongod start
 
 # Install cpanm 
